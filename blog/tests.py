6591c36fde4b86d40dc801fa27fe682d9a120cc4
Ajay Wajirwad <ajay.wajirwad@sas.com>
2024-05-21 16:34:03 +0530

    new change

--- blog/tests.py
+++ blog/tests.py
@@ -9,5 +9,6 @@ class ModelTesting(TestCase):
 
     def test_post_model(self):
         d = self.blog
+        # Test
         self.assertTrue(isinstance(d, Post))
         self.assertEqual(str(d), 'django')
